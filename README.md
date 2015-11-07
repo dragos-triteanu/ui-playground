521fb0c46d2d6c242101488488c7b9e8ad6421f2
dragos-triteanu <dragos.triteanu@gmail.com>
2015-11-07 14:17:08 +0200

    added google closure compiler to the project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Javascript example project for fast lookup on best practices.
+
+
+For GCC help :
+java -jar compiler.jar --help
+
+Compilation levels:
+WHITESPACE_ONLY
+SIMPLE
+ADVANCED
+
+For Google Closure compiler :
+java -jar compiler.jar 'src/**.js' --js_output_file=out.js --compilation_level=ADVANCED
+
--- src/theJS.js
+++ src/theJS.js
@@ -1,17 +1,41 @@
 /*global console*/
+
+/** @const */
+var SOME_CONSTANT = 'constant';
+
+/**
+ * @enum {string}
+ */
+var POSSIBILITY = {
+      YES: 1,
+      NOT: -1,
+      MAYBE: 0
+   };
+
+/** @constructor
+ *  @param {number} id The id of the element.
+ *  @param {string|number|null} name The name of the element.
+ * */
 function SomeObj(id, name) {
    var
-      _name = name,
-      _id = id;
+      /** @type {number} */
+      _id = id,
+      /** @type {string} */
+      _name = name;
 
+   /** @return {string|number|null} The name of the element */
    this.getName = function () {return _name; };
 
+    /** @param {string|number|null} newName */
    this.setName = function (newName) { _name = newName; };
 
+    /** @return {number} The id of the element */
    this.getId = function () {return _id; };
 
+    /** @param {number} newId */
    this.setId = function (newId) { _id = newId; };
 
+    /** @return {string} The string representation of an object */
    this.toString = function () { return "SomeObj : {'id'=" + _id + ",'name'=" + _name + "}"; };
 
 }
